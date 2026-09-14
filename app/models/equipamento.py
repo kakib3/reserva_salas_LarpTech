09276c9e67f70af9b172232425b40d6baa923d3b
João Miguel <joaomiguelgalvaoferrazd@gmail.com>
2026-09-14 17:44:44 -0400

    feat: models de usuario, sala e equipamento criados

---
+++ app/models/equipamento.py
@@ -0,0 +1,5 @@
+class Equipamento:
+    def __init__(self, id, nome, status):
+        self.id = id
+        self.nome = nome
+        self.status = status
---
+++ app/models/sala.py
@@ -0,0 +1,9 @@
+class Sala: 
+    def __init__(self, id, nome, capacidade, predio, andar, status, tipo_sala):
+        self.id = id
+        self.nome = nome
+        self.capacidade = capacidade
+        self.predio = predio
+        self.andar = andar
+        self.status = status
+        self.tipo_sala = tipo_sala
