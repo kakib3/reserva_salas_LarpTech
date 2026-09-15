c4cf62f8d4daede06b050e6837e257936ac41dae
João Miguel <joaomiguelgalvaoferrazd@gmail.com>
2026-09-15 18:30:01 -0400

    feat: implementa regra de negócio da criação de uma reserva e testes da regra

---
+++ app/services/reservas_service.py
@@ -0,0 +1,100 @@
+import pandas as pd
+
+from app.models.dados import DATA_DIR
+from app.models.reserva import Reserva
+from datetime import date
+
+
+def carregar_salas():
+    arquivo = DATA_DIR / "salas.csv"
+    return pd.read_csv(arquivo, sep=";")
+
+
+def verificar_sala_disponivel(id_sala):
+    salas = carregar_salas()
+
+    sala = salas[salas["idSala"] == id_sala]
+
+    if sala.empty:
+        return False
+
+    return sala.iloc[0]["status"] == "Disponivel"
+
+def verificar_conflito_horario(id_sala, data, hora_inicio, hora_fim):
+    arquivo = DATA_DIR / "reservas.csv"
+    reservas = pd.read_csv(arquivo, sep=";")
+    
+    reservas["data"] = pd.to_datetime(
+        reservas["data"],
+        format="%d/%m/%Y"
+    ).dt.date
+    
+    reservas["horaInicio"] = pd.to_datetime(
+        reservas["horaInicio"],
+        format="mixed"
+    ).dt.time
+
+    reservas["horaFim"] = pd.to_datetime(
+        reservas["horaFim"],
+        format="mixed"
+    ).dt.time
+
+    mesma_sala_e_data = (
+        (reservas["idSala"] == id_sala)
+        & (reservas["data"] == data) 
+        & (reservas["status"].isin(["Confirmada", "Pendente"]))
+    )
+    sobreposicao = (reservas["horaInicio"] < hora_fim) & (reservas["horaFim"] > hora_inicio)
+
+    return not reservas[mesma_sala_e_data & sobreposicao].empty
+
+def criar_reserva(id_usuario, id_sala, data, hora_inicio, hora_fim):
+    arquivo = DATA_DIR / "reservas.csv"
+    
+    if not verificar_sala_disponivel(id_sala):
+        return False  # Sala indisponível
+    
+    reserva_obj = Reserva(None, id_usuario, id_sala, data, hora_inicio, hora_fim, None, None)
+    
+    reserva_obj.validar_horario()
+    reserva_obj.validar_intervalo()
+    reserva_obj.validar_horario_funcionamento()
+    reserva_obj.validar_dia_util()
+    
+    if verificar_conflito_horario(id_sala, data, hora_inicio, hora_fim):
+        return False  # Conflito de horário
+    
+    sala_tipo = descobre_tipo_sala(id_sala)
+    
+    if(sala_tipo == "LABORATORIO" or sala_tipo == "AUDITORIO"):
+        reserva_obj.status = "Pendente"
+    elif(sala_tipo == "SALA" or sala_tipo == "REUNIAO"):
+        reserva_obj.status = "Confirmada"
+        
+    reservas = pd.read_csv(arquivo, sep=";")
+    novo_id = reservas["idReserva"].max() + 1 if not reservas.empty else 1
+    reserva_obj.criado_em = date.today()
+    reserva_obj.id = novo_id
+    
+    nova_reserva = {
+    "idReserva": reserva_obj.id,
+    "idUser": reserva_obj.usuario_id,
+    "idSala": reserva_obj.sala_id,
+    "data": reserva_obj.data.strftime("%d/%m/%Y"),
+    "horaInicio": reserva_obj.hora_inicio.strftime("%H:%M"),
+    "horaFim": reserva_obj.hora_fim.strftime("%H:%M"),
+    "status": reserva_obj.status,
+    "criado_em": reserva_obj.criado_em.strftime("%d/%m/%Y"),
+    }
+    
+    nova_linha = pd.DataFrame([nova_reserva])
+    reservas = pd.concat([reservas, nova_linha], ignore_index=True)
+    reservas.to_csv(arquivo, sep=";", index=False)
+    return reserva_obj
+    
+def descobre_tipo_sala(id_sala):
+    salas = carregar_salas()
+    sala = salas[salas["idSala"] == id_sala]
+    if not sala.empty:
+        return sala.iloc[0]["tipoSala"]
+    return None
---
+++ tests/test_reservas_service.py
@@ -0,0 +1,173 @@
+import unittest
+import shutil
+
+from datetime import date, time
+
+from app.models.dados import DATA_DIR
+from app.services.reservas_service import criar_reserva
+
+
+class TestCriarReserva(unittest.TestCase):
+
+    def setUp(self):
+        self.arquivo_reservas = DATA_DIR / "reservas.csv"
+        self.backup_reservas = DATA_DIR / "reservas_backup.csv"
+
+        shutil.copy(self.arquivo_reservas, self.backup_reservas)
+
+    def tearDown(self):
+        shutil.copy(self.backup_reservas, self.arquivo_reservas)
+        self.backup_reservas.unlink()
+
+    def test_criar_reserva_valida(self):
+        reserva = criar_reserva(
+            1,
+            2,
+            date(2026, 9, 16),
+            time(19, 0),
+            time(21, 0)
+        )
+
+        self.assertIsNot(False, reserva)
+
+    def test_criar_reserva_com_conflito(self):
+        criar_reserva(
+            1,
+            2,
+            date(2026, 9, 16),
+            time(19, 0),
+            time(21, 0)
+        )
+
+        reserva = criar_reserva(
+            3,
+            2,
+            date(2026, 9, 16),
+            time(20, 0),
+            time(22, 0)
+        )
+
+        self.assertIs(reserva, False)
+
+    def test_duracao_menor_que_30_minutos(self):
+        with self.assertRaises(ValueError):
+            criar_reserva(
+                1,
+                2,
+                date(2026, 9, 16),
+                time(19, 0),
+                time(19, 20)
+            )
+
+    def test_duracao_maior_que_4_horas(self):
+        with self.assertRaises(ValueError):
+            criar_reserva(
+                1,
+                2,
+                date(2026, 9, 16),
+                time(17, 0),
+                time(22, 0)
+            )
+
+    def test_horario_inicio_igual_ao_fim(self):
+        with self.assertRaises(ValueError):
+            criar_reserva(
+                1,
+                2,
+                date(2026, 9, 16),
+                time(19, 0),
+                time(19, 0)
+            )
+
+    def test_horario_fora_do_funcionamento(self):
+        with self.assertRaises(ValueError):
+            criar_reserva(
+                1,
+                2,
+                date(2026, 9, 16),
+                time(6, 0),
+                time(8, 0)
+            )
+
+    def test_reserva_no_domingo(self):
+        with self.assertRaises(ValueError):
+            criar_reserva(
+                1,
+                2,
+                date(2026, 9, 20),
+                time(19, 0),
+                time(21, 0)
+            )
+
+    def test_sala_em_manutencao(self):
+        reserva = criar_reserva(
+            1,
+            7,
+            date(2026, 9, 16),
+            time(19, 0),
+            time(21, 0)
+        )
+
+        self.assertIs(reserva, False)
+
+    def test_sala_indisponivel(self):
+        reserva = criar_reserva(
+            1,
+            10,
+            date(2026, 9, 16),
+            time(19, 0),
+            time(21, 0)
+        )
+
+        self.assertIs(reserva, False)
+
+    def test_sala_inexistente(self):
+        reserva = criar_reserva(
+            1,
+            999,
+            date(2026, 9, 16),
+            time(19, 0),
+            time(21, 0)
+        )
+
+        self.assertIs(reserva, False)
+
+    def test_sala_comum_fica_confirmada(self):
+        reserva = criar_reserva(
+            1,
+            2,
+            date(2026, 9, 16),
+            time(19, 0),
+            time(21, 0)
+        )
+
+        self.assertIsNot(False, reserva)
+        self.assertEqual(reserva.status, "Confirmada")
+
+    def test_laboratorio_fica_pendente(self):
+        reserva = criar_reserva(
+            1,
+            6,
+            date(2026, 9, 16),
+            time(19, 0),
+            time(21, 0)
+        )
+
+        self.assertIsNot(False, reserva)
+        self.assertEqual(reserva.status, "Pendente")
+
+    def test_auditorio_fica_pendente(self):
+        reserva = criar_reserva(
+            1,
+            11,
+            date(2026, 9, 16),
+            time(19, 0),
+            time(21, 0)
+        )
+
+        self.assertIsNot(False, reserva)
+        self.assertEqual(reserva.status, "Pendente")
+
+
+if __name__ == "__main__":
+    unittest.main()
